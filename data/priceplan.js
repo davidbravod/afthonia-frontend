dada6a830e0dcf4d5e9e86502c74d0b1dbe5c4a8
David Bravo <mac@Davids-MacBook-Air.local>
2023-11-08 18:12:56 -0500

    team added and pricing change

--- data/priceplan.js
+++ data/priceplan.js
@@ -7,21 +7,21 @@ export const priceplan = [
         id:1, 
         title: 'Freelancer Plan',
         price: 99,
-        features: ['LLC Formation', 'Registered Agent', 'EIN Application', 'Keep your privacy, use our address', 'Same-day documents scanning', 'Mail Forwarding', '24/7 Virtual Office', 'Compliance Notifications', 'Banking Application'],
+        features: ['LLC Formation', 'Registered Agent', 'Keep your privacy, use our address', 'Same-day documents scanning', 'Mail Forwarding', '24/7 Virtual Office', 'Compliance Notifications'],
         bg:globe
     },
     {
         id:2, 
         title: 'Small Business Plan',
-        price: 299,
-        features: ['All Freelancer Plan PLUS:', 'Operating Agreement for LLCs', 'Bylaws for Corporations', 'Contract Templates', 'Domain Register', 'Unlimited Business Emails', 'Email Autoresponder', 'Web Hosting', 'Wordpress Website Builder'],
+        price: 199,
+        features: ['All Freelancer Plan PLUS:', 'Same-day Filing', 'Same-day EIN Application', 'Operating Agreement for LLCs', 'Bylaws for Corporations', 'Contract Templates', 'Bank Application'],
         bg:rocket
     },
     {
         id:3, 
         title: 'Enterprise Plan',
         price: 499,
-        features: ['All Small Business Plan PLUS:', 'Email Campaigns Powered by AI', 'Social Media Posts Powered by AI', 'Courses Library', '3 Consultation Calls', 'IRS Filing Forms 5472 and 1120'],
+        features: ['All Small Business Plan PLUS:', 'Domain Register', 'Unlimited Business Emails', 'Email Auto Responder', 'Web Hosting', 'Website Builder', 'SSL Certificate'],
         bg: union
     },
 ]
